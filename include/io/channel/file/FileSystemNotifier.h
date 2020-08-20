17471da86e6062cb65da394adad2cea7427971db
anarion <untrac10100@gmail.com>
2020-08-20 22:05:36 +0800

    begin file system notifier

---
+++ include/io/channel/file/FileSystemNotifier.h
@@ -0,0 +1,35 @@
+//
+// Created by anarion on 8/20/20.
+//
+
+#ifndef ANBASE_FILESYSTEMNOTIFIER_H
+#define ANBASE_FILESYSTEMNOTIFIER_H
+
+#include <SystemException.h>
+#include <container/List/Vector.hpp>
+#include <sys/epoll.h>
+#include <concurrent/base/Thread.h>
+#include "FileChannel.h"
+
+namespace anarion {
+class FileSystemNotifier : public Thread {
+protected:
+    int ifd, epfd;
+
+    void addEpoll(int ops, int fd, struct epoll_event &event);
+    void addInotifier(const char *path, int ops);
+public:
+    FileSystemNotifier();
+    ~FileSystemNotifier();
+
+    void addFileChannel(const FileChannel &fileChannel);
+
+    [[noreturn]] void run() override ;
+};
+struct NotifierException : public SystemException {};
+struct NotifierInitFailed : public NotifierException {};
+struct NotifierPollFailed : public NotifierException {};
+struct NotifierAppendListenFailed : public NotifierException {};
+}
+
+#endif //ANBASE_FILESYSTEMNOTIFIER_H
---
+++ src/io/channel/file/FileSystemNotifier.cpp
@@ -0,0 +1,57 @@
+//
+// Created by anarion on 8/20/20.
+//
+
+#include "io/channel/file/FileSystemNotifier.h"
+#include <sys/inotify.h>
+#include <unistd.h>
+
+anarion::FileSystemNotifier::FileSystemNotifier() {
+    // init inotify
+    ifd = inotify_init();
+    if (ifd == -1) {
+        throw NotifierInitFailed();
+    }
+    // init epoll
+    epfd = epoll_create(1);
+    if (epfd < 0) {
+        throw NotifierInitFailed();
+    }
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = ifd;
+    addEpoll(0, ifd, event);
+}
+
+[[noreturn]] void anarion::FileSystemNotifier::run() {
+    const size_type bufferSize = 81;
+    char buf[bufferSize];
+    while (true) {
+        struct epoll_event event;
+        int num = epoll_wait(epfd, &event, 1, -1);
+        if (num > 0) {
+            // read off the message
+            int msgLen = read(ifd, buf, bufferSize);
+            printf("%.*s\n", msgLen, buf);
+        }
+    }
+}
+
+void anarion::FileSystemNotifier::addEpoll(int ops, int fd, epoll_event &event) {
+    epoll_ctl(epfd, ops | EPOLL_CTL_ADD, fd, &event);
+}
+
+void anarion::FileSystemNotifier::addInotifier(const char *path, int ops) {
+    int wd = inotify_add_watch(ifd, path, ops);
+    if (wd < 0) {
+        throw NotifierAppendListenFailed();
+    }
+}
+
+anarion::FileSystemNotifier::~FileSystemNotifier() {
+
+}
+
+void anarion::FileSystemNotifier::addFileChannel(const anarion::FileChannel &fileChannel) {
+    addInotifier(fileChannel.getAbsolutePath().cstr(), IN_ACCESS);
+}
