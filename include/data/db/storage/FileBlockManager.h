3d067472b513f953a1ac1c9c7b2e8e89fccad787
Anarion-zuo <untrac10100@gmail.com>
2020-12-09 16:32:58 +0800

    free block management

--- include/data/db/storage/FileBlockManager.h
+++ include/data/db/storage/FileBlockManager.h
@@ -6,6 +6,7 @@
 #define ANBASE_FILEBLOCKMANAGER_H
 
 #include <io/channel/file/FileChannel.h>
+#include <container/List/BitArray.h>
 #include "typedecl.h"
 
 namespace anarion {
@@ -15,28 +16,44 @@ class FileBlockManager {
 public:
     static const blockno_t null = -1;
 protected:
-    Vector<FileChannel *> blockFiles;
-    size_type blockSize;
+    struct BlockInfo {
+        FileChannel *file;
+        BitArray usedMap;
+        blockoff_t minUnusedIndex;
 
-    void allocateNewBlockFile(const Path &dir);
+        BlockInfo(FileChannel *file, blockoff_t blockSize, blockoff_t pageSize) : file(file), usedMap(blockSize / pageSize), minUnusedIndex(0) {}
+
+        bool allocate(blockoff_t &pageIndex);
+        void deallocate(blockoff_t pageIndex);
+    };
+    Vector<BlockInfo> blockFiles;
+    blockoff_t blockSize, pageSize;
+    Path fileDir;
+
+    blockno_t allocateNewBlockFile(const Path &dir);
 
     FileChannel *fetchBlockChannel(blockno_t index);
 
 public:
-    explicit FileBlockManager(size_type blockSize, blockno_t blockInitCount = 1);
-    explicit FileBlockManager(const Path &dir, size_type blockSize, blockno_t blockInitCount = 1);
+    explicit FileBlockManager(const Path &dir, size_type blockSize, blockoff_t pageSize, blockno_t blockInitCount = 1);
     ~FileBlockManager();
 
     constexpr size_type getBlockSize() const { return blockSize; }
+    constexpr blockoff_t getPageSize() const { return pageSize; }
     constexpr blockno_t getBlockCount() const { return blockFiles.size(); }
     constexpr size_type getTotalSize() const { return getBlockSize() * blockFiles.size(); }
 
     void out(blockno_t blockno, blockoff_t offset, char *buffer, size_type length);
     void in(blockno_t blockno, blockoff_t offset, const char *buffer, size_type length);
+
+    void allocateFreePage(blockno_t &blockno, blockoff_t &blockoff);
+    void deallocatePage(blockno_t blockno, blockoff_t blockoff);
+
+    struct Exception : public std::exception {};
+    struct OutOfIndex : public Exception {};
 };
 
-struct DataBlockManagerException : public std::exception {};
-struct DataBlockOperationOutOfIndex : public DataBlockManagerException {};
+
 }
 }
 
--- src/data/db/FileBlockManager.cpp
+++ src/data/db/FileBlockManager.cpp
@@ -4,44 +4,40 @@
 
 #include "data/db/storage/FileBlockManager.h"
 
-anarion::db::FileBlockManager::FileBlockManager(anarion::size_type blockSize, blockno_t blockInitCount)
-:    blockSize(blockSize), blockFiles(blockInitCount) {
-
-}
-
-void anarion::db::FileBlockManager::allocateNewBlockFile(const Path &dir) {
+anarion::db::blockno_t anarion::db::FileBlockManager::allocateNewBlockFile(const Path &dir) {
     size_type blockno = blockFiles.size();
     FileOpenFlag oflag;
     oflag.setReadWrite();
     oflag.setCreateIfNotExists();
     FileChannel *channel = new FileChannel(Path::combine(dir, Path(SString::parseDec(blockno))), oflag);
-    blockFiles.pushBack(channel);
+    blockFiles.pushBack(BlockInfo(channel, blockSize, pageSize));
     channel->open(S_IRWXU);
+    return blockno;
 }
 
 anarion::db::FileBlockManager::~FileBlockManager() {
     for (size_type index = 0; index < blockFiles.size(); ++index) {
-        delete blockFiles.get(index);
+        delete blockFiles.get(index).file;
     }
 }
 
-anarion::db::FileBlockManager::FileBlockManager(const anarion::Path &dir, anarion::size_type blockSize, blockno_t blockInitCount)
-    : FileBlockManager(blockSize, blockInitCount) {
+anarion::db::FileBlockManager::FileBlockManager(const anarion::Path &dir, anarion::size_type blockSize, blockoff_t pageSize, blockno_t blockInitCount)
+    : blockSize(blockSize), blockFiles(blockInitCount), pageSize(pageSize), fileDir(dir) {
     for (size_type index = 0; index < blockInitCount; ++index) {
         allocateNewBlockFile(dir);
     }
 }
 
 anarion::FileChannel *anarion::db::FileBlockManager::fetchBlockChannel(blockno_t index) {
-    return blockFiles.get(index);
+    return blockFiles.get(index).file;
 }
 
 void anarion::db::FileBlockManager::out(blockno_t blockno, blockoff_t offset, char *buffer, anarion::size_type length) {
     if (offset >= blockSize) {
-        throw DataBlockOperationOutOfIndex();
+        throw OutOfIndex();
     }
     if (offset + length > blockSize) {
-        throw DataBlockOperationOutOfIndex();
+        throw OutOfIndex();
     }
     if (buffer == nullptr) {
         return;
@@ -56,10 +52,10 @@ void anarion::db::FileBlockManager::out(blockno_t blockno, blockoff_t offset, ch
 
 void anarion::db::FileBlockManager::in(blockno_t blockno, blockoff_t offset, const char *buffer, anarion::size_type length) {
     if (offset >= blockSize) {
-        throw DataBlockOperationOutOfIndex();
+        throw OutOfIndex();
     }
     if (offset + length > blockSize) {
-        throw DataBlockOperationOutOfIndex();
+        throw OutOfIndex();
     }
     if (buffer == nullptr) {
         return;
@@ -70,4 +66,54 @@ void anarion::db::FileBlockManager::in(blockno_t blockno, blockoff_t offset, con
     FileChannel *file = fetchBlockChannel(blockno);
     file->setCursor(offset);
     file->in(buffer, length);
+}
+
+void
+anarion::db::FileBlockManager::allocateFreePage(anarion::db::blockno_t &blockno, anarion::db::blockoff_t &blockoff) {
+    blockoff_t pageIndex;
+    for (blockno_t index = 0; index < blockFiles.size(); ++index) {
+        BlockInfo &block = blockFiles.get(index);
+        if (block.allocate(pageIndex)) {
+            // free page obtained
+            blockno = index;
+            blockoff = pageIndex * pageSize;
+            return;
+        } else {
+            // must look at another block
+        }
+    }
+    // must allocate a new block
+    blockno = allocateNewBlockFile(fileDir);
+    blockFiles.get(blockno).allocate(pageIndex);
+    blockoff = pageIndex * pageSize;
+}
+
+void anarion::db::FileBlockManager::deallocatePage(anarion::db::blockno_t blockno, anarion::db::blockoff_t blockoff) {
+    blockFiles.get(blockno).deallocate(blockoff / pageSize);
+}
+
+bool anarion::db::FileBlockManager::BlockInfo::allocate(anarion::db::blockoff_t &pageIndex) {
+    blockoff_t ret = minUnusedIndex;
+    if (ret >= usedMap.getLength()) {
+        return false;
+    }
+    usedMap.set(ret);
+    pageIndex = ret;
+    while (true) {
+        ++minUnusedIndex;
+        if (minUnusedIndex >= usedMap.getLength()) {
+            break;
+        }
+        if (!usedMap.check(minUnusedIndex)) {
+            break;
+        }
+    }
+    return true;
+}
+
+void anarion::db::FileBlockManager::BlockInfo::deallocate(anarion::db::blockoff_t pageIndex) {
+    usedMap.clear(pageIndex);
+    if (pageIndex < minUnusedIndex) {
+        minUnusedIndex = pageIndex;
+    }
 }
